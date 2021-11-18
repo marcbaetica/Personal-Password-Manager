f8a2894728ce67b7dad1d7005de8c784eb212690
Marc Baetica <marc.baetica@gmail.com>
2021-11-17 23:57:48 -0500

    Nbits range for encryption keys.

--- lib/encryption.py
+++ lib/encryption.py
@@ -3,7 +3,10 @@ import rsa
 
 class Encryption:
     @staticmethod
-    def generate_keys(nbits):
+    def generate_keys(nbits=882):
+        # 82 bits are used as buffer by rsa
+        # The remaining 800 bits (100 bytes) will allow encryption of a strings of up to 100 characters.
+        # Anything under 16 bits will throw ValueError.
         return rsa.newkeys(nbits)
 
     @staticmethod
