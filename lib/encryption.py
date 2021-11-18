a07e09edce24fab541a01bd3b781553652719af4
Marc Baetica <marc.baetica@gmail.com>
2021-11-18 00:21:44 -0500

    Finished Encryption lib.

--- lib/encryption.py
+++ lib/encryption.py
@@ -6,7 +6,9 @@ class Encryption:
     def generate_keys(nbits=882):
         # 82 bits are used as buffer by rsa
         # The remaining 800 bits (100 bytes) will allow encryption of a strings of up to 100 characters.
-        # Anything under 16 bits will throw ValueError.
+        # Anything under 16 bits will throw ValueError: Key too small.
+        # Anything from 16 bits and under 82 bits will raise:
+        # OverflowError: X bytes needed for message (your char count), but there is only space for -9 (at 16) -1 (at 81)
         return rsa.newkeys(nbits)
 
     @staticmethod
@@ -16,3 +18,17 @@ class Encryption:
     @staticmethod
     def decrypt(crypto, priv_key):
         return rsa.decrypt(crypto, priv_key).decode()
+
+    @staticmethod
+    def get_key_as_string(key):
+        return key.save_pkcs1().decode()
+
+    @classmethod
+    def save_key_to_file(cls, key, name):
+        with open(f'{name}', 'w') as f:
+            f.write(cls.get_key_as_string(key))
+
+    @staticmethod
+    def load_key_from_file(name):
+        with open(f'{name}', 'r') as f:
+            return f.read()
--- main.py
+++ main.py
@@ -1,56 +1,20 @@
 from lib.encryption import Encryption
 
 
-
 message = 'Hello'
-# pub, priv = Encryption.generate_keys(512)
-# print(pub.save_pkcs1())
-# print(priv.save_pkcs1())
-#
-# crypto = Encryption.encrypt(message, pub)
-# decrypted = Encryption.decrypt(crypto, priv)
-#
-# print(message)
-# print(crypto.hex())
-# print(decrypted)
+pub, priv = Encryption.generate_keys()
+print(Encryption.get_key_as_string(pub))
+print(Encryption.get_key_as_string(priv))
 
+Encryption.save_key_to_file(pub, 'public_key')
+print(Encryption.load_key_from_file('public_key'))
 
-def run_permutation(message, nbits):
-    pub, priv = Encryption.generate_keys(nbits)
-    # print(len(pub.save_pkcs1().decode()))
-    # print(priv.save_pkcs1())
+Encryption.save_key_to_file(priv, 'private_key')
+print(Encryption.load_key_from_file('private_key'))
 
-    crypto = Encryption.encrypt(message, pub)
-    decrypted = Encryption.decrypt(crypto, priv)
+crypto = Encryption.encrypt(message, pub)
+decrypted = Encryption.decrypt(crypto, priv)
 
-    print(message)
-    print(crypto.hex())
-    print(decrypted)
-
-
-import string
-from random import choice
-
-
-message = ''.join([choice(string.ascii_uppercase) for _ in range(100)])
 print(message)
-for i in range(870, 890):
-    try:
-        run_permutation(message, i)
-        print(f'Number of bits: {i}')
-    except ValueError:
-        print(f'{i} not acceptable.')
-    except OverflowError as e:
-        print(f'{i} not acceptable. Overflow error: {e}')
-
-
-# 15 not acceptable.
-# 16 not acceptable. Overflow error: 0 bytes needed for message, but there is only space for -9
-#  -> 0 chars, 82 bits key (10.25 bytes)
-# H -> 1 char, 90 bits key (11.25 bytes)
-# He -> 2 chars, 98 bits key (12.25 bytes)
-# Hel -> 3 chars, 106 bits key (13.25 bytes)
-# Hell -> 4 chars, 114 bits key (14.25 bytes)
-# Hello -> 5 chars, 122 bits key (15.25 bytes)
-# 4096 n-bits => 501 chars max.
-# 100 chars + 10.25 buffer = 110.25 chars. 110.25 * 8 bits = 882 bits will encrypt strings of up to 100 characters.
+print(crypto.hex())
+print(decrypted)
