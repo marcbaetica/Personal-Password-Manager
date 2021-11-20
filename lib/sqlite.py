0c0faba5c1fb6e33e3941a130f5be2ea7e447d86
Marc Baetica <marc.baetica@gmail.com>
2021-11-19 21:16:56 -0500

    Structured queries

--- lib/sqlite.py
+++ lib/sqlite.py
@@ -5,26 +5,43 @@ conn = sqlite3.connect('credentials.db')
 
 cursor = conn.cursor()
 
-# cursor.execute("""CREATE TABLE credentials(
-#                     site TEXT,
-#                     user TEXT,
-#                     password TEXT
-#                 )""")
 
-# cursor.execute("INSERT INTO credentials VALUES ('some_site', 'user1', 'some_pass')")
+def create_db_and_table():
+    cursor.execute("""CREATE TABLE IF NOT EXISTS credentials(
+                        site TEXT,
+                        user TEXT,
+                        password TEXT
+                    )""")
 
-# cursor.execute("SELECT * FROM credentials")
-# cursor.execute("SELECT * FROM credentials WHERE site='some_site'")
-# cursor.execute("SELECT * FROM credentials WHERE site='some_siteaa'")
 
-# print(cursor.fetchall())
+def insert_credential_into_table(site, user, password):
+    cursor.execute(f"INSERT INTO credentials VALUES ('{site}', '{user}', '{password}'")
+    conn.commit()
+
+
+def return_all_credentials():
+    cursor.execute("SELECT * FROM credentials")
+    cursor.fetchall()
+
+
+def return_credentials_for_site(site):
+    cursor.execute(f"SELECT * FROM credentials WHERE site={site}")
+    cursor.fetchall()
+
 # fetchone() (returns None if no more rows available) and fetchmany(5) (returns max items as list)
 
 # returns an iterable of type <class 'sqlite3.Cursor'>
 
-data = cursor.execute("SELECT * FROM credentials WHERE site='some_site'")
-print(type(data))
-print([item for item in data])
+# data = cursor.execute("SELECT * FROM credentials WHERE site='some_site'")
+# print(type(data))  # <class 'sqlite3.Cursor'>
+# print([item for item in data])
+# [('some_site', 'user1', 'some_pass'), ('some_site', 'user1', 'some_pass')]
 
+
+create_db_and_table()
+insert_credential_into_table('some_site', 'user1', 'some_pass')
+print(return_all_credentials())
 conn.commit()
+conn.close()
+
 
